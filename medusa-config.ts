fcacec843b35ac1e4a334aa2ead902ed9bba0e36
Franco Lucero <frlcr@icloud.com>
2025-01-31 15:31:04 -0300

    Fix CORS

--- medusa-config.ts
+++ medusa-config.ts
@@ -8,8 +8,8 @@ module.exports = defineConfig({
     databaseUrl: process.env.DATABASE_URL,
     redisUrl: process.env.REDIS_URL,
     http: {
-      storeCors: "*",
-      adminCors: "*",
+      storeCors: process.env.STORE_CORS!,
+      adminCors: process.env.ADMIN_CORS!,
       authCors: process.env.AUTH_CORS!,
       jwtSecret: process.env.JWT_SECRET ?? "supersecret",
       cookieSecret: process.env.COOKIE_SECRET ?? "supersecret",
